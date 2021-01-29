cc14291c7394fdee43cde5c8d7f810a3dfa9b1a0
ovkhasch <vkolegkv@gmail.com>
2021-01-29 04:57:09 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,12 @@
 variable "instance_type" {
   type = string
   description = "Instance type"
-  default = "t2.nano"
+  default = "t2.micro"
+}
+
+variable "image_id" {
+  type = string
+  description = "AMI id"
+  default = ""
 }
 
