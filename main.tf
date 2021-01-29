d7aaf3bbc65c3d999bdd63d0d35b725e04f4daf4
ovkhasch <vkolegkv@gmail.com>
2021-01-29 04:57:35 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-25488752"
+  ami                    = var.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = ["${aws_security_group.web.id}"]
   user_data              = "${file("template/user_data.sh")}"
